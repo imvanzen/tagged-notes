33b1aa953f81ca2fb5f1803d9831f61d152c543f
J.Reczko <reczko.jakub@gmail.com>
2022-11-29 10:49:18 +0100

    Havigate after save is done

--- src/components/NoteFrom.tsx
+++ src/components/NoteFrom.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from "react"
 import { Button, Col, Form, Row, Stack } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import CreatebleReactSelect from "react-select/creatable"
 import { NoteData, Tag } from "../types"
 import { v4 as uuidV4 } from "uuid"
@@ -15,6 +15,7 @@ function NoteFrom ({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    const navigate = useNavigate()
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
@@ -23,6 +24,7 @@ function NoteFrom ({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
             markdown: markdownRef.current!.value,
             tags: selectedTags
         })
+        navigate('..')
     }
 
     return (
