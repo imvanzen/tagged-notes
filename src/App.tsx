41fa89b45c72ba40c9cba2617fef7e6334188952
J.Reczko <reczko.jakub@gmail.com>
2022-11-29 08:31:18 +0100

    Create note form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
+import {Routes, Route, Navigate} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import NewNote from './components/NewNote'
 
+function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<h1>Hi</h1>} />
+        <Route path='/new' element={<NewNote />} />
+        <Route path='/:id'> 
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Container>
   )
 }
 
---
+++ src/components/NewNote.tsx
@@ -0,0 +1,12 @@
+import NoteFrom from "./NoteFrom"
+
+function NewNote () {
+    return (
+        <>
+            <h1 className="mb-4">New Note</h1>
+            <NoteFrom />
+        </>
+    )
+}
+
+export default NewNote
